c636862d92e10b7fb8f99080f60f7073ac5f8d4d
Robert Amorim <robertamorimbot@gmail.com>
2023-01-24 16:49:21 -0300

    Organizando as branchs

--- src/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const app = express();
-app.use(express.json());
-
-app.post('/activities', (_req, res) => res.status(201).json({ message: "Atividade cadastrada com sucesso!" }));
-
-module.exports = app;
